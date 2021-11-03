4015933e69dd1dd375f3ec6c2c0f4ee460a19f64
Rusty Porter <rusty.porter@gmail.com>
2021-11-03 14:06:20 -0700

    Connected database to project
    Using Sequelize, as well as created Player table

--- server/index.js
+++ server/index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 
 const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Server is performing live in front of ${PORT} fans!`);
-});
+
+const dbConnection = require("./db/index");
+
+const startServer = async () => {
+  await dbConnection.sync();
+  app.listen(PORT, () => {
+    console.log(`Server is performing live in front of ${PORT} fans!`);
+  });
+};
+startServer();
 
 app.get("/", (req, res) => {
   res.send("Hey, how are ya?");
